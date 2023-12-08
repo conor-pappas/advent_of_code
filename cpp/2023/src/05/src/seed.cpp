dd72275f2ef47a9ec5e030587fc34143581ef7eb
Conor Pappas <conor.pappas@gmail.com>
2023-12-08 01:47:24 -0500

    [AoC] Day 5 Part 2 - Refactoring to prepare for implementation.

--- cpp/2023/src/05/src/seed.cpp
+++ cpp/2023/src/05/src/seed.cpp
@@ -55,104 +55,144 @@ struct Descriptor {
     };
 };
 
-class DescriptorRangeTransform {
+class DescriptorRange {
 public:
-    DescriptorRangeTransform(Descriptor input, Descriptor output, const long size):
-        input_range_start(std::move(input)),
-        output_range_start(std::move(output)),
-        size(size)
-    {};
+    DescriptorRange(Descriptor range_start, const long size):
+        _range_start(std::move(range_start)),
+        _size(size) {
+        assert(size > 0);
+    };
 
-    [[nodiscard]] long input_start() const {
-        return input_range_start.value;
-    }
+    [[nodiscard]] long start() const { return _range_start.value; }
+    [[nodiscard]] long end() const { return _range_start.value + size() - 1; }
+    [[nodiscard]] Category category() const { return _range_start.category; }
+    [[nodiscard]] bool empty() const { return size() == 0;}
+    [[nodiscard]] long size() const { return _size; }
 
-    [[nodiscard]] long input_end() const {
-        return input_range_start.value + size - 1;
-    }
-
-    [[nodiscard]] long output_start() const {
-        return output_range_start.value;
-    }
-
-    [[nodiscard]] long output_end() const {
-        return output_range_start.value + size;
-    }
-
-    [[nodiscard]] Category input_category() const {
-        return input_range_start.category;
-    }
-
-    [[nodiscard]] Category output_category() const {
-        return output_range_start.category;
-    }
-
-    friend ostream& operator<<(ostream& os, const DescriptorRangeTransform& d) {
-        return os << d.input_category() << "(" << d.input_start() << "-" << d.input_end() << ") -> "
-            << d.output_category() << "(" << d.output_start() << "-" << d.output_end() << ")";
-    }
-
-    Descriptor operator()(const Descriptor& input) const {
-        if(!in_range(input)) {
-            throw std::runtime_error("Input out of range");
-        }
-        return Descriptor{output_range_start.category, input.value - input_start() + output_start()};
+    [[nodiscard]] DescriptorRange intersect(const DescriptorRange& other) const {
+        assert(other.category() == category());
+        const long range_start = max(start(), other.start());
+        const long range_end = min(end(), other.end());
+        const long range_size = range_end - range_start + 1;
+        return DescriptorRange({category(), range_start}, range_size);
     }
 
     [[nodiscard]] bool in_range(const Descriptor& input) const {
-        assert(input.category == input.category);
-        return input.value >= input_start() && input.value <= input_end();
+        assert(input.category == category());
+        return input.value >= start() && input.value <= end();
     }
 
+    friend ostream& operator<<(ostream& os, const DescriptorRange& d) {
+        return os << d.category() << "(" << d.start() << "-" << d.end() << ")";
+    };
 private:
-    Descriptor input_range_start;
-    Descriptor output_range_start;
-    long size = 0;
+    Descriptor _range_start;
+    long _size;
 };
 
-class DescriptorRangeTransformIndex {
+class DescriptorRangeTransform {
 public:
-    Category input_category;
-    Category output_category;
-    vector<DescriptorRangeTransform> transforms{};
+    DescriptorRange domain_range;
+    DescriptorRange image_range;
 
-    DescriptorRangeTransformIndex(Category input_category, Category output_category):
-        input_category(std::move(input_category)),
-        output_category(std::move(output_category))
-    {}
+    DescriptorRangeTransform(DescriptorRange domain_range, DescriptorRange image_range):
+        domain_range(std::move(domain_range)),
+        image_range(std::move(image_range)) {
+        assert(domain_range.category() == image_range.category());
+        assert(domain_range.size() == image_range.size());
+    };
 
-    void add(const DescriptorRangeTransform& transform) {
-        assert(transform.input_category() == input_category);
-        assert(transform.output_category() == output_category);
-        transforms.push_back(transform);
-    }
-
-    // TODO: actually use an interval tree
-    [[nodiscard]] optional<DescriptorRangeTransform> find(const Descriptor& input) const {
-        assert(input.category == input_category);
-        auto in_range = [input](const DescriptorRangeTransform& transform) {
-            return transform.in_range(input);
-        };
-        const auto result = ranges::find_if(transforms, in_range);
-        return result == transforms.end() ? nullopt : make_optional(*result);
+    [[nodiscard]] long offset() const {
+        return image_range.start() - domain_range.start();
     }
 
     Descriptor operator()(const Descriptor& input) const {
-        if (const auto transform = find(input)) {
-            return transform.value()(input);
+        assert(in_range(input));
+        return Descriptor{image_range.category(), input.value + offset()};
+    }
+
+    DescriptorRange operator()(const DescriptorRange& input) const {
+        const auto intersection = domain_range.intersect(input);
+        return { {intersection.category(), intersection.start()}, intersection.size() };
+    }
+
+    [[nodiscard]] bool in_range(const Descriptor& input) const {
+        return domain_range.in_range(input);
+    }
+
+    friend ostream& operator<<(ostream& os, const DescriptorRangeTransform& d) {
+        return os << d.domain_range << " -> " << d.image_range;
+    }
+};
+
+class DescriptorSet {
+public:
+    void insert(const DescriptorRange& other) {
+        if(other.empty()) { return; }
+        // TODO: assert that other is not a subset of any existing range
+        ranges.insert(other);
+    }
+
+    void insert(const DescriptorSet& other) {
+        ranges.insert(other.ranges.begin(), other.ranges.end());
+    }
+private:
+    struct DescriptorRangeCompare {
+        bool operator() (const DescriptorRange& lhs, const DescriptorRange& rhs) const {
+            return lhs.end() < rhs.start();
+        }
+    };
+    set<DescriptorRange, DescriptorRangeCompare> ranges;
+};
+
+class DescriptorSetTransform {
+public:
+    void add(const DescriptorRangeTransform& transform) {
+        assert(transform.domain_range.category() == _domain_category);
+        assert(transform.image_range.category() == _image_category);
+        transforms.push_back(transform);
+    }
+
+    DescriptorSetTransform(Category domain_category, Category image_category):
+        _domain_category(std::move(domain_category)),
+        _image_category(std::move(image_category))
+    {}
+
+    [[nodiscard]] Category domain_category() const { return _domain_category; }
+    [[nodiscard]] Category image_category() const { return _image_category; }
+
+    Descriptor operator()(const Descriptor& input) const {
+        auto result = transforms
+            | views::filter([&](const auto& transform) { return transform.in_range(input); })
+            | views::transform([&](const auto& transform) { return transform(input); });
+        if (result.empty()) {
+            return {_image_category, input.value};
         } else {
-            return {output_category, input.value};
+            assert(distance(result.begin(), result.end()) == 1);
+            return *result.begin();
         }
     }
+
+    DescriptorSet operator()(const DescriptorRange& input) const {
+        DescriptorSet result;
+        for (const auto& transform : transforms) {
+            result.insert(transform(input));
+        }
+        return result;
+    }
+private:
+    Category _domain_category;
+    Category _image_category;
+    vector<DescriptorRangeTransform> transforms{};
 };
 
 const regex SEED_FORMAT = regex(R"(seeds: (\d+( \d+)*))");
 
-vector<Descriptor> parseSeeds(string& line) {
+vector<Descriptor> parseSeeds(const string& line) {
     vector<Descriptor> descriptors;
     smatch match;
     if (regex_match(line, match, SEED_FORMAT)) {
-        string seeds = match[1].str();
+        const string seeds = match[1].str();
         for (const auto& seed :split(seeds, ' ')) {
             string str(seed.begin(), seed.end());
             descriptors.push_back(Descriptor{"seed", stol(string(seed.begin(), seed.end()))});
@@ -165,25 +205,25 @@ const regex DESCRIPTOR_TRANSFORM_HEADER_FORMAT = regex(R"((\w+)-to-(\w+) map:)")
 const regex DESCRIPTOR_TRANSFORM_MAP_FORMAT = regex(R"((\d+) (\d+) (\d+))");
 
 
-DescriptorRangeTransformIndex parseDescriptorMap(span<string> lines) {
+DescriptorSetTransform parseDescriptorSetTransform(span<string> lines) {
     string header = lines[0];
     smatch headerMatch;
     if (!regex_search(header, headerMatch, DESCRIPTOR_TRANSFORM_HEADER_FORMAT)) {
         throw runtime_error("Invalid descriptor transform header");
     }
-    const Category input_category = headerMatch[1];
-    const Category output_category = headerMatch[2];
+    const Category domain_category = headerMatch[1];
+    const Category image_category = headerMatch[2];
 
-    DescriptorRangeTransformIndex transform_index(input_category, output_category);
+    DescriptorSetTransform transform_index(domain_category, image_category);
     for(const string& line : lines.subspan(1)) {
         smatch mapMatch;
         if (regex_search(line, mapMatch, DESCRIPTOR_TRANSFORM_MAP_FORMAT)) {
-            const long output_value = stol(mapMatch[1]);
-            const long input_value = stol(mapMatch[2]);
+            const long image_start_value = stol(mapMatch[1]);
+            const long domain_start_value = stol(mapMatch[2]);
             const long size = stol(mapMatch[3]);
-            const Descriptor input = {input_category, input_value};
-            const Descriptor output = {output_category, output_value};
-            const DescriptorRangeTransform transform = {input, output, size};
+            const DescriptorRange domain_range = {{domain_category, domain_start_value}, size };
+            const DescriptorRange image_range = {{image_category, image_start_value}, size};
+            const DescriptorRangeTransform transform = {domain_range, image_range};
             transform_index.add(transform);
         }
     }
@@ -191,7 +231,7 @@ DescriptorRangeTransformIndex parseDescriptorMap(span<string> lines) {
     return transform_index;
 }
 
-using Almanac = unordered_map<Category, DescriptorRangeTransformIndex>;
+using Almanac = unordered_map<Category, DescriptorSetTransform>;
 
 Almanac parseAlmanac(const span<string>& lines) {
     Almanac almanac;
@@ -201,16 +241,16 @@ Almanac parseAlmanac(const span<string>& lines) {
         smatch headerMatch;
         const bool header = regex_search(curLine, headerMatch, DESCRIPTOR_TRANSFORM_HEADER_FORMAT);
         if(header && !mapLines.empty()) {
-            const auto transformIndex = parseDescriptorMap(mapLines);
-            almanac.insert({transformIndex.input_category, transformIndex});
+            const auto transformIndex = parseDescriptorSetTransform(mapLines);
+            almanac.insert({transformIndex.domain_category(), transformIndex});
             mapLines.clear();
         }
         if(!curLine.empty()) {
             mapLines.push_back(curLine);
         }
     }
-    const auto transformIndex = parseDescriptorMap(mapLines);
-    almanac.insert({transformIndex.input_category, transformIndex});
+    const auto transformIndex = parseDescriptorSetTransform(mapLines);
+    almanac.insert({transformIndex.domain_category(), transformIndex});
     return almanac;
 }
 
@@ -220,7 +260,9 @@ long part_1(const vector<Descriptor>& seeds, const Almanac& almanac) {
     for (auto const &seed : seeds) {
         Descriptor seed_result = seed;
         while(seed_result.category != "location") {
-            seed_result = almanac.at(seed_result.category)(seed_result);
+            const auto& transform = almanac.at(seed_result.category);
+            const auto new_seed_result = transform(seed_result);
+            seed_result = new_seed_result;
         }
         seed_locations.insert(seed_result.value);
     }
