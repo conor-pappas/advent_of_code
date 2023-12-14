9be94458531294b871526a82eff50ea5e2b667ed
Conor Pappas <conor.pappas@gmail.com>
2023-12-13 20:06:07 -0500

    [AoC] Day 6 Part 2

--- cpp/2023/src/06/src/race.cpp
+++ cpp/2023/src/06/src/race.cpp
@@ -61,23 +61,6 @@ public:
     }
 };
 
-class BoatState {
-public:
-    time_t time { 0 };
-    distance_t position { 0 };
-    speed_t speed { 0 };
-
-    [[nodiscard]] static BoatState hold_button(const time_t time) {
-        return { time, 0, static_cast<speed_t>(time) };
-    }
-};
-
-// TODO: not really needed - we are going to just use a fixed formula
-void update(BoatState state, const time_t time = 1) {
-    state.time += time;
-    state.position += state.speed * time;
-}
-
 const regex TIMES_REGEX = regex(R"(Time:\s*(\d+(\s+\d+)*))");
 const regex DISTANCES_REGEX = regex(R"(Distance:\s*(\d+(\s+\d+)*))");
 vector<Record> parse_records(const span<string> lines) {
@@ -107,27 +90,65 @@ vector<Record> parse_records(const span<string> lines) {
     return records;
 }
 
+Record parse_record(const span<string>& lines) {
+    assert(lines.size() == 2);
+
+    vector<time_t> times;
+    vector<distance_t> distances;
+
+    smatch times_match, distances_match;
+    if (!regex_match(lines[0], times_match, TIMES_REGEX)) { throw runtime_error("Invalid times"); }
+    if (!regex_match(lines[1], distances_match, DISTANCES_REGEX)) { throw runtime_error("Invalid distances"); }
+
+    string time_str, distance_str;
+    for (const auto& time : split(times_match[1].str(), ' ')) {
+        time_str += time;
+    }
+    for (const auto& distance : split(distances_match[1].str(), ' ')) {
+        distance_str += distance;
+    }
+
+    return { stol(time_str), stol(distance_str) };
+}
+
 distance_t distance_for_button(const time_t& hold_time, const time_t& race_duration) {
     return (race_duration - hold_time) * static_cast<speed_t>(hold_time);
 }
 
-long part_1(const vector<Record>& records) {
-    long result = 1;
-    for (const auto& [time, distance] : records) {
-        long num_records = 0;
-        for (long i = 1; i < time; i++) {
-            if(distance_for_button(i, time) > distance) {
-                num_records++;
-            }
+time_t num_records_brute(const Record& record) {
+    time_t num_records = 0;
+    for (time_t i = 1; i < record.time; i++) {
+        if(distance_for_button(i, record.time) > record.distance) {
+            num_records++;
         }
-        result *= num_records;
+    }
+
+    return num_records;
+}
+
+time_t num_records_algebraic(const Record& record) {
+    const double discriminant = sqrt((record.time * record.time) - (4 * (record.distance + 1)));
+    const time_t max = floor((static_cast<double>(record.time) + discriminant) / 2);
+    const time_t min = ceil((static_cast<double>(record.time) - discriminant) / 2);
+
+    return max - min + 1;
+}
+
+time_t num_records(const Record& record) {
+   return num_records_algebraic(record);
+}
+
+time_t part_1(const vector<Record>& records) {
+    time_t result = 1;
+    for (const Record& record : records) {
+        result *= num_records(record);
     }
 
     return result;
 }
 
-long part_2() {
-    return 0;
+time_t part_2(const Record& record) {
+    return num_records((record));
 }
 
 int main(const int argc, char** argv) {
@@ -136,10 +157,8 @@ int main(const int argc, char** argv) {
     const span<string> lines_span = lines;
 
     const vector<Record> records = parse_records(lines_span);
+    const Record record = parse_record(lines_span);
 
-    for (const auto& record : records) {
-        cout << record << endl;
-    }
     cout << "Part 1: " << part_1(records) << endl;
-    cout << "Part 2: " << part_2() << endl;
+    cout << "Part 2: " << part_2(record) << endl;
 }
